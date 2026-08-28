02fcf0a42658c5ddcaa28af7fb1ca6f00d401747
Aayush Walsangikar <aayushwalsangikar118@gmail.com>
2026-08-29 01:33:41 +0530

    Time: 2 ms (66.05%), Space: 43.3 MB (37.06%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,20 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        int[] count = new int[128];
+
+        for (char c : s.toCharArray()) {
+            count[c]++;
+        }
+
+        for (char c : t.toCharArray()) {
+            count[c]--;
+
+            if (count[c] < 0) {
+                return c;
+            }
+        }
+
+        return ' ';
+    }
+}
